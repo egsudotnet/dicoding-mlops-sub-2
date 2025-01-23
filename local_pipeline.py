0bb4da4f8ebff340583001c4b02ac3e17ef68573
egsu <egsudotnet@gmail.com>
2025-01-23 17:31:16 +0700

    -

--- local_pipeline.py
+++ local_pipeline.py
@@ -1,4 +1,6 @@
-# memanggil berbagai library dan variabel yang dibutuhkan
+# Menjalankan Komponen Machine Learning Pipeline
+
+## Pada bagian awal tentunya kita perlu memanggil berbagai library dan variabel yang dibutuhkan seperti berikut.
 
 import os
 import sys
@@ -8,12 +10,12 @@ from absl import logging
 from tfx.orchestration import metadata, pipeline
 from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
  
-PIPELINE_NAME = "patient-stress-pipeline"
+PIPELINE_NAME = "customer-churn-pipeline"
  
 # pipeline inputs
 DATA_ROOT = "data"
-TRANSFORM_MODULE_FILE = "modules/patient_stress_transform.py"
-TRAINER_MODULE_FILE = "modules/patient_stress_trainer.py"
+TRANSFORM_MODULE_FILE = "modules/customer_churn_transform.py"
+TRAINER_MODULE_FILE = "modules/customer_churn_trainer.py"
 # requirement_file = os.path.join(root, "requirements.txt")
  
 # pipeline outputs
@@ -22,8 +24,7 @@ serving_model_dir = os.path.join(OUTPUT_BASE, 'serving_model')
 pipeline_root = os.path.join(OUTPUT_BASE, PIPELINE_NAME)
 metadata_path = os.path.join(pipeline_root, "metadata.sqlite")
 
-# menyatukan seluruh TFX component
-# Fungsi di atas akan menghasilkan sebuah pipeline yang dijalankan menggunakan Apache Beam. Pipeline tersebut menggunakan mode multi processing dengan menyesuaikan jumlah CPU yang tersedia.
+## Membuat sebuah fungsi bernama init_local_pipeline(). Fungsi inilah yang digunakan untuk menyatukan seluruh TFX component. Untuk menjalankan tugasnya, ia membutuhkan dua buah parameter input, yaitu components dan pipeline_root.
 
 def init_local_pipeline(
     components, pipeline_root: Text
@@ -48,8 +49,7 @@ def init_local_pipeline(
         eam_pipeline_args=beam_args
     )
 
-# menjalankan pipeline
-# membuat sebuah TFX component menggunakan fungsi init_components(). Selanjutnya, seluruh komponen tersebut disatukan menjadi sebuah machine learning pipeline dengan bantuan fungsi init_local_pipeline(). Pada bagian terakhir, pipeline tersebut dijalankan menggunakan Apache Beam sebagai pipeline orchestrator.
+# Untuk menjalankan pipeline tersebut, tambahkanlah perintah berikut.
 if __name__ == "__main__":
     logging.set_verbosity(logging.INFO)
     
@@ -67,9 +67,4 @@ if __name__ == "__main__":
     pipeline = init_local_pipeline(components, pipeline_root)
     BeamDagRunner().run(pipeline=pipeline)
 
-# Untuk menjalankan berkas local_pipeline.py, tulislah perintah berikut pada terminal atau windows PowerShell
 # python .\local_pipeline.py 
-
-#.\mlops-tfx-dev-ops\Scripts\activate
-# where python
-# "E:\ProgramData\anaconda3\envs\mlops-tfx-dev-ops\python.exe" local_pipeline.py
